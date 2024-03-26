37b3ae480ee1d05af64aaf1419c4445619dc6ae1
VarunKapoor0 <varunkap@usc.edu>
2024-03-26 15:57:41 -0700

    Ui Changes(Minor)

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class Restart : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -21,5 +19,14 @@ public class Restart : MonoBehaviour
     {
         SceneManager.LoadScene(0);
         //gameObject.SetActive(false);
+    }
+
+    public void WinCondition()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void TryAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
